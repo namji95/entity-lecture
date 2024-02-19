6cb0e6cb3fbf994c248298325e657b9ef7bb11bb
namji <skawlgns07@naver.com>
2024-02-19 23:07:27 +0900

    feat: 1대 1 양방향 테스트
    entity 패키지와 repository 패키지 해당 클래스를 생성 후 test 코드 작성하여 1대 1 양방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,4 +13,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "user")
+    private Food food;
+
+    public void addFood(Food food) {
+        this.food = food;
+        food.setUser(this);
+    }
 }
+
+/*
+1대 1 단방향 테스트 할 때 사용
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+ */
