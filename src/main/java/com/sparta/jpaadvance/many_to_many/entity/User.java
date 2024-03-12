33f808f0d0aee5111c60c7e276fb98a2daad13ee
namji <skawlgns07@naver.com>
2024-03-12 16:53:02 +0900

    feat: Two way many to many relationship

--- src/main/java/com/sparta/jpaadvance/many_to_many/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/many_to_many/entity/User.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +20,15 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+
+  // 양방향 시 필요
+  @ManyToMany(mappedBy = "userList")
+  private List<Food> foodList = new ArrayList<>();
+
+  public void addFoodList(Food food) {
+    this.foodList.add(food);
+    food.getUserList().add(this); // 외래 키(연관 관계) 설정
+  }
 }
 
 /*
