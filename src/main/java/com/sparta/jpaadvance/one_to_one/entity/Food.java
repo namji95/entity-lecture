a2816b5ef7b1ff645a5e2442bf74231f91dc6acd
namji <skawlgns07@naver.com>
2024-03-12 18:52:15 +0900

    feat: Lazy loading Immediate loading Test And Remove unnecessary imports

--- src/main/java/com/sparta/jpaadvance/one_to_one/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/one_to_one/entity/Food.java
@@ -1,6 +1,12 @@
 package com.sparta.jpaadvance.one_to_one.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/com/sparta/jpaadvance/one_to_one/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/one_to_one/entity/User.java
@@ -1,8 +1,11 @@
 package com.sparta.jpaadvance.one_to_one.entity;
 
-import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
